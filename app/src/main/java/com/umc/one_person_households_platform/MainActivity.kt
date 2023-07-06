62c1f03330519a6d80f14fda70ea72725024e6de
KJH <gh7052@gmail.com>
2023-07-06 23:19:53 +0900

    Initial commit

---
+++ app/src/main/java/com/umc/one_person_households_platform/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.umc.one_person_households_platform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
