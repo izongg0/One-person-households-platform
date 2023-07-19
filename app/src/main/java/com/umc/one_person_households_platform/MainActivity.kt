a94a0f2739b1f361bc23158edea0790184170423
KJH <gh7052@gmail.com>
2023-07-19 19:41:32 +0900

    feature: 메인 화면 UI 구현

--- app/src/main/java/com/umc/one_person_households_platform/MainActivity.kt
+++ app/src/main/java/com/umc/one_person_households_platform/MainActivity.kt
@@ -2,10 +2,22 @@ package com.umc.one_person_households_platform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.umc.one_person_households_platform.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // navHost 찾기
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_container) as NavHostFragment
+        // BottomNavigationView 에게 NavController 설정
+        binding.bnvMoveFragment.setupWithNavController(navHostFragment.navController)
     }
 }
