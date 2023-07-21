9627ec4aa663607d2bd2fa5b07b27169981b941e
jonghun <qweasdfdsa11@naver.com>
2023-07-22 01:43:15 +0900

    feature: 커뮤니티 UI 구현

---
+++ app/src/main/java/com/umc/one_person_households_platform/community/LatestFragment.kt
@@ -0,0 +1,28 @@
+package com.umc.one_person_households_platform.community
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.umc.one_person_households_platform.R
+
+
+class LatestFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_latest, container, false)
+    }
+
+
+}
