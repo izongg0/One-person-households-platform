7e68316d479625da81258bf2c784263536fa2771
KJH <gh7052@gmail.com>
2023-07-25 18:38:39 +0900

    feature: 공동 구매 검색 화면 UI 구현

---
+++ app/src/main/java/com/umc/one_person_households_platform/view/groupbuying/GroupBuyingSearchFragment.kt
@@ -0,0 +1,32 @@
+package com.umc.one_person_households_platform.view.groupbuying
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.umc.one_person_households_platform.databinding.FragmentGroupBuyingSearchBinding
+
+class GroupBuyingSearchFragment : Fragment() {
+
+    private var _binding: FragmentGroupBuyingSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGroupBuyingSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
