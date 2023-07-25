79fdd31b3e9bad778ab84783647a8e618094fc69
jonghun <qweasdfdsa11@naver.com>
2023-07-25 23:48:25 +0900

    feature: 커뮤니티 카테고리 선택기능 구현

---
+++ app/src/main/java/com/umc/one_person_households_platform/model/CommunityPostlistDTO.kt
@@ -0,0 +1,7 @@
+package com.umc.one_person_households_platform.model
+
+data class CommunityPostlistDTO (
+    var category : String ,
+    var postName : String ,
+    var author : String,
+)
--- app/src/main/java/com/umc/one_person_households_platform/view/community/CommunityFragment.kt
+++ app/src/main/java/com/umc/one_person_households_platform/view/community/CommunityFragment.kt
@@ -1,14 +1,23 @@
-package com.umc.one_person_households_platform.view.community
+package com.umc.one_person_households_platform.community
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.one_person_households_platform.R
+import com.umc.one_person_households_platform.adapter.CommunityCategoryAdapter
+import com.umc.one_person_households_platform.databinding.FragmentCommunityBinding
+import com.umc.one_person_households_platform.model.CommunityPostlistDTO
 
 
 class CommunityFragment : Fragment() {
+
+
+    private lateinit var binding : FragmentCommunityBinding
+    lateinit var postAdapter: CommunityCategoryAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +30,95 @@ class CommunityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_community,container,false)
+
+        var latestbtn = binding.btnLatest
+        var popularbtn = binding.btnPopular
+        var delicious = binding.btnDelicious
+        var question = binding.btnQuestion
+        var communicate = binding.btnCommunicate
+
+        // 테스트 데이터
+        var postList : ArrayList<CommunityPostlistDTO> = arrayListOf()
+        var firstauthor = "aaaa"
+        var firstcategory = "최신글"
+        var firstname = "최신글 테스트 _111"
+        var latest_1  = CommunityPostlistDTO(firstcategory,firstname,firstauthor)
+        var secondauthor = "bbbb"
+        var secondcategory = "최신글"
+        var secondname = "최신글 테스트 2222"
+        var latest_2 = CommunityPostlistDTO(secondcategory,secondname,secondauthor)
+
+        postList.add(latest_1)
+        postList.add(latest_2)
+
+        postAdapter = CommunityCategoryAdapter(postList)
+        binding.rvPostlist.adapter = postAdapter
+        binding.rvPostlist.layoutManager = LinearLayoutManager(requireContext())
+
+        latestbtn.setOnClickListener {
+
+            // 테스트 데이터
+            var postList : ArrayList<CommunityPostlistDTO> = arrayListOf()
+            var firstauthor = "aaaa"
+            var firstcategory = "최신글"
+            var firstname = "최신글 테스트 _111"
+            var latest_1  = CommunityPostlistDTO(firstcategory,firstname,firstauthor)
+            var secondauthor = "aaaa"
+            var secondcategory = "최신글"
+            var secondname = "최신글 테스트 _111"
+            var latest_2 = CommunityPostlistDTO(secondcategory,secondname,secondauthor)
+
+            postList.add(latest_1)
+            postList.add(latest_2)
+
+            postAdapter = CommunityCategoryAdapter(postList)
+            binding.rvPostlist.adapter = postAdapter
+            binding.rvPostlist.layoutManager = LinearLayoutManager(requireContext())
+
+
+
+
+        }
+        popularbtn.setOnClickListener {
+
+            // 테스트 데이터
+            var postList_2 : ArrayList<CommunityPostlistDTO> = arrayListOf()
+            var firstauthor_2 = "aaaa"
+            var firstcategory_2 = "인기글"
+            var firstname_2 = "인기글 테스트 _111"
+            var pupular_1  = CommunityPostlistDTO(firstcategory_2,firstname_2,firstauthor_2)
+            var secondauthor_2 = "bbbb"
+            var secondcategory_2 = "인기글"
+            var secondname_2 = "인기글 테스트 _111"
+            var pupular_2 = CommunityPostlistDTO(secondcategory_2,secondname_2,secondauthor_2)
+
+            postList_2.add(pupular_1)
+            postList_2.add(pupular_2)
+
+            postAdapter = CommunityCategoryAdapter(postList_2)
+            binding.rvPostlist.adapter = postAdapter
+            binding.rvPostlist.layoutManager = LinearLayoutManager(requireContext())
+
+
+        }
+        delicious.setOnClickListener {
+
+        }
+
+        question.setOnClickListener {
+
+        }
+        communicate.setOnClickListener {
+
+        }
+
+
+
+
+
+
+        return binding.root
     }
 
 
