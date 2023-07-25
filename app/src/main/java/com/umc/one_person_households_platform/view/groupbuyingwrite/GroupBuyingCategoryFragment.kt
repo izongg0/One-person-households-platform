7814474a40942d6a46444f92df165f896a2c5e1a
KJH <gh7052@gmail.com>
2023-07-25 19:09:50 +0900

    feature: 공동 구매 쓰기 카테고리 선택 화면 UI 구현

---
+++ app/src/main/java/com/umc/one_person_households_platform/view/groupbuyingwrite/GroupBuyingCategoryFragment.kt
@@ -0,0 +1,32 @@
+package com.umc.one_person_households_platform.view.groupbuyingwrite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.umc.one_person_households_platform.databinding.FragmentGroupBuyingCategoryBinding
+
+class GroupBuyingCategoryFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentGroupBuyingCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGroupBuyingCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
