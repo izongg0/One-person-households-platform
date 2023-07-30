0ae1e615c92f2e95dd6dbd0df42cad2f948c5146
jonghun <qweasdfdsa11@naver.com>
2023-07-30 17:23:12 +0900

    feature: 커뮤니티 게시글 작성 유효성 검사 추가 및 사진 첨부기능 구현

--- app/src/main/java/com/umc/one_person_households_platform/view/community/NewpostFragment.kt
+++ app/src/main/java/com/umc/one_person_households_platform/view/community/NewpostFragment.kt
@@ -1,13 +1,24 @@
 package com.umc.one_person_households_platform.view.community
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.umc.one_person_households_platform.R
+import com.umc.one_person_households_platform.databinding.FragmentNewpostBinding
 
 class NewpostFragment : Fragment() {
+
+    private lateinit var
+            binding: FragmentNewpostBinding
+    private val PICK_IMAGE_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +28,70 @@ class NewpostFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_newpost, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_newpost, container, false)
+
+        binding.tvCatergory.setOnClickListener {
+            val orderBottomDialogFragment: NewpostcatergoryFragment = NewpostcatergoryFragment() {
+                when (it) {
+                    0 -> binding.tvCatergory.text = "맛집이야기"
+                    1 -> binding.tvCatergory.text = "질문있어요"
+                    2 -> binding.tvCatergory.text = "대화해요"
+                    3 -> binding.tvCatergory.text = "공유해요"
+
+                }
+            }
+            orderBottomDialogFragment.show(parentFragmentManager, orderBottomDialogFragment.tag)
+        }
+
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                // 모든 EditText의 입력 내용이 비어있지 않은지 확인
+                val isEditText1NotEmpty = !binding.etEditcontent.text.isNullOrBlank()
+                val isEditText2NotEmpty = !binding.etEdittitle.text.isNullOrBlank()
+                val isEditText3NotEmpty = !(binding.tvCatergory.text == "게시글의 주제를 선택해주세요")
+
+                // 모든 EditText가 비어있지 않으면 버튼 활성화
+                binding.tvAddbtn.isEnabled =
+                    isEditText1NotEmpty && isEditText2NotEmpty && isEditText3NotEmpty
+
+            }
+        }
+        binding.etEditcontent.addTextChangedListener(textWatcher)
+        binding.etEdittitle.addTextChangedListener(textWatcher)
+        binding.tvCatergory.addTextChangedListener(textWatcher)
+
+
+        //추가
+
+
+        binding.tvAddphoto.setOnClickListener {
+            openGallery()
+        }
+
+        return binding.root
+
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            data?.data?.let { uri ->
+                val bitmap =
+                    MediaStore.Images.Media.getBitmap(requireContext().contentResolver, uri)
+                binding.ivPhoto.setImageBitmap(bitmap)
+            }
+        }
     }
 }
 
