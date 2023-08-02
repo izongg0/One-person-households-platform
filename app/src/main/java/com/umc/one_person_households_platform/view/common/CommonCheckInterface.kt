3873ea6438cddfe6a7215257919c32e5f6c9cf6a
KJH <gh7052@gmail.com>
2023-08-02 19:09:21 +0900

    refactor: 중복되는 다이얼로그 관련 파일 간소화

---
+++ app/src/main/java/com/umc/one_person_households_platform/view/common/CommonCheckInterface.kt
@@ -0,0 +1,5 @@
+package com.umc.one_person_households_platform.view.common
+
+interface CommonCheckInterface {
+    fun onCheckButtonClick()
+}
---
+++ app/src/main/java/com/umc/one_person_households_platform/view/common/CommonYesOrNoInterface.kt
@@ -0,0 +1,6 @@
+package com.umc.one_person_households_platform.view.common
+
+interface CommonYesOrNoInterface {
+    fun onYesButtonClick()
+    fun onNoButtonClick()
+}
