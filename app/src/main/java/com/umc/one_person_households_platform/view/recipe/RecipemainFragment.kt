372b51dd22e98d604256bfef4e96e06adf696c73
jonghun <qweasdfdsa11@naver.com>
2023-08-13 18:47:59 +0900

    feature: 레시피 리스트 api 연결

--- app/src/main/java/com/umc/one_person_households_platform/view/recipe/RecipemainFragment.kt
+++ app/src/main/java/com/umc/one_person_households_platform/view/recipe/RecipemainFragment.kt
@@ -1,15 +1,33 @@
 package com.umc.one_person_households_platform.view.recipe
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.one_person_households_platform.R
+import com.umc.one_person_households_platform.adapter.CommunityCategoryAdapter
+import com.umc.one_person_households_platform.adapter.RecipeListAdapter
+import com.umc.one_person_households_platform.databinding.FragmentCommunityBinding
+import com.umc.one_person_households_platform.databinding.FragmentRecipemainBinding
+import com.umc.one_person_households_platform.model.CommunityDTO
+import com.umc.one_person_households_platform.model.RecipeDTO
+import com.umc.one_person_households_platform.network.ApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class RecipemainFragment : Fragment() {
 
+
+    private lateinit var
+            binding: FragmentRecipemainBinding
+
+    lateinit var recipeAdapter: RecipeListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +39,57 @@ class RecipemainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recipemain, container, false)
+
+        callApi("최신순")
+
+        binding.rbLatest.setOnClickListener{
+
+
+            callApi("최신순")
+        }
+        binding.rbPopular.setOnClickListener{
+
+
+            callApi("인기순")
+        }
 
 
 
-        return inflater.inflate(R.layout.fragment_recipemain, container, false)
+        return binding.root
+    }
+
+
+    fun callApi(sort: String) {
+        var recipelist: RecipeDTO? = null
+
+        val apiClient = ApiClient.create()
+
+        apiClient.getRecipe(sort).enqueue(object : Callback<RecipeDTO> {
+            override fun onResponse(call: Call<RecipeDTO>, response: Response<RecipeDTO>) {
+                if (response.isSuccessful) {
+                    recipelist = response.body()
+
+                    Log.d("tttt",recipelist.toString())
+                    if (recipelist != null) {
+
+                        recipeAdapter = RecipeListAdapter(recipelist!!)
+                        binding.rvRecipelist.adapter = recipeAdapter
+                        binding.rvRecipelist.layoutManager = LinearLayoutManager(requireContext())
+
+                    }
+
+
+                } else {
+                    Log.e("ApiError", "API 요청 실패: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<RecipeDTO>, t: Throwable) {
+                Log.e("ApiError", "API 요청 실패", t)
+            }
+        })
+
     }
 
 
