3d165f294855a6854ebb8354666b326dfdddec98
KJH <gh7052@gmail.com>
2023-08-20 23:57:36 +0900

    feature: 공동 구매 글쓰기 카테고리 선택, 모집 인원 선택 화면 기능 구현

--- app/src/main/java/com/umc/one_person_households_platform/view/groupbuyingwrite/GroupBuyingWriteCategoryFragment.kt
+++ app/src/main/java/com/umc/one_person_households_platform/view/groupbuyingwrite/GroupBuyingWriteCategoryFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.umc.one_person_households_platform.databinding.FragmentGroupBuyingWriteCategoryBinding
 
@@ -11,6 +13,8 @@ class GroupBuyingWriteCategoryFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentGroupBuyingWriteCategoryBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: GroupBuyingWriteShareViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +27,20 @@ class GroupBuyingWriteCategoryFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    // 초기 설정
+    private fun init() {
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.category = this
+    }
+
+    // 카테고리 클릭
+    fun onCategoryButton(text: String) {
+        viewModel.setGroupBuyingCategory(text)
+        findNavController().navigateUp()
     }
 
     override fun onDestroyView() {
